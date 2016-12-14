dbb77c484a713b1fc64c308f17e136fe1950c947
jhu4 <jhu4@wpi.edu>
2016-12-14 15:25:33 -0500

    format the rank table

--- js/top10.js
+++ js/top10.js
@@ -2,7 +2,6 @@
 //update the top 10 list
 var name = 'tempUser';
 var score = 403;
-
 var top10State = {
 	preload: function(){
 		console.log('In top10State');
@@ -11,13 +10,13 @@ var top10State = {
 
 	create: function(){
 
+		this.top10table = game.add.sprite(75,50,'top10table');
 		this.names = game.add.group();
 		this.scores = game.add.group();
 		//this.sendResult();
 		this.generateList();
 		this.getList();
 
-		game.add.image(75,50,'top10table');
 		game.add.button(125,500,'back2button',this.backToMenu,this,0,1,2);
 	},
 
@@ -25,9 +24,9 @@ var top10State = {
 		var i = 1;
 
 		for(i =1; i < 11; i+=1){
-			var nameLabel = game.add.text(30, 5 + 30 * (i-1), 'name' + i,
+			var nameLabel = game.add.text(100, 35*(i-1)+105, 'name' + i,
 			{ font: '18px Arial', fill: '#826484' });
-			var scoreLabel = game.add.text(120, 5 + 30 * (i-1), 'score' + i,
+			var scoreLabel = game.add.text(220, 35*(i-1)+105, 'score' + i,
 			{ font: '18px Arial', fill: '#826484' });
 
 			this.names.add(nameLabel);
